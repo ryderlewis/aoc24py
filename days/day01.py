168f8abdcc9349a9d53ed81e3f557590b5e42fa3
Ryder Lewis <ryder.lewis@gmail.com>
2024-11-30 23:03:24 -0800

    day 1

--- days/day01.py
+++ days/day01.py
@@ -1,4 +1,5 @@
 from .day import Day
+from collections import Counter
 
 
 class Day01(Day):
@@ -6,7 +7,21 @@ class Day01(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        nums = [list(map(int, line.split())) for line in self.data_lines()]
+        l1 = sorted([x[0] for x in nums])
+        l2 = sorted([x[1] for x in nums])
+        dist = 0
+        for i1, i2 in zip(l1, l2):
+            dist += abs(i2-i1)
+        return str(dist)
+
 
     def part2(self) -> str:
-        return "dayXX 2"
+        nums = [list(map(int, line.split())) for line in self.data_lines()]
+        l1 = [x[0] for x in nums]
+        l2 = Counter([x[1] for x in nums])
+        sim = 0
+        for i in l1:
+            sim += i * l2[i]
+        return str(sim)
+
