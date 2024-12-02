f69ca18b3b4e77630386eca31ecfc2d6944db1b8
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-01 22:15:31 -0800

    day 2

--- days/day02.py
+++ days/day02.py
@@ -6,7 +6,22 @@ class Day02(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        safe_count = 0
+        for line in self.data_lines():
+            n = list(map(int, line.split()))
+            if (all(1 <= n[i] - n[i-1] <= 3 for i in range(1, len(n))) or
+                    all(1 <= n[i-1] - n[i] <= 3 for i in range(1, len(n)))):
+                safe_count += 1
+        return str(safe_count)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        safe_count = 0
+        for line in self.data_lines():
+            nums = list(map(int, line.split()))
+            for x in range(len(nums)):
+                n = nums[:x] + nums[x+1:]
+                if (all(1 <= n[i] - n[i-1] <= 3 for i in range(1, len(n))) or
+                        all(1 <= n[i-1] - n[i] <= 3 for i in range(1, len(n)))):
+                    safe_count += 1
+                    break
+        return str(safe_count)
