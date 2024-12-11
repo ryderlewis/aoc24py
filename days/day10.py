6f982f40d971ec0fc50a99c493a6e813f97214d2
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-10 20:09:00 -0800

    refactor day 10

--- days/day10.py
+++ days/day10.py
@@ -10,44 +10,18 @@ class Day10(Day):
         super().__init__(*args, **kwargs)
 
     @staticmethod
-    def score(grid: list[list[int]], trailhead: Pos) -> int:
-        work = deque([(trailhead, 0)])
-        seen = {trailhead}
-        score = 0
-        while work:
-            pos, elevation = work.popleft()
-            if elevation == 9:
-                score += 1
-                continue
-            for direction in (Pos(-1, 0), Pos(1, 0), Pos(0, 1), Pos(0, -1)):
-                test_pos = Pos(pos.row+direction.row, pos.col+direction.col)
-                if test_pos in seen:
-                    continue
-                if 0 <= test_pos.row < len(grid) and 0 <= test_pos.col < len(grid[0]) and grid[test_pos.row][test_pos.col] == elevation + 1:
-                    seen.add(test_pos)
-                    work.append((test_pos, elevation + 1))
-        return score
-
-    def part1(self) -> str:
-        grid = self.parse()
-        score = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == 0:
-                    score += self.score(grid, Pos(row, col))
-        return str(score)
-
-    @staticmethod
-    def rating(grid: list[list[int]], trailhead: Pos) -> int:
+    def score(grid: list[list[int]], trailhead: Pos) -> tuple[int, int]:
         work: deque[tuple[tuple[Pos, ...], int]] = deque([((trailhead,), 0)])
         seen: set[tuple[Pos, ...]] = {(trailhead,)}
-        score = 0
+        unique_ends = set()
+        rating = 0
         while work:
             trail, elevation = work.popleft()
-            if elevation == 9:
-                score += 1
-                continue
             pos = trail[-1]
+            if elevation == 9:
+                rating += 1
+                unique_ends.add(pos)
+                continue
             for direction in (Pos(-1, 0), Pos(1, 0), Pos(0, 1), Pos(0, -1)):
                 test_pos = Pos(pos.row+direction.row, pos.col+direction.col)
                 test_trail: tuple[Pos, ...] = trail + (test_pos,)
@@ -56,16 +30,25 @@ class Day10(Day):
                 if 0 <= test_pos.row < len(grid) and 0 <= test_pos.col < len(grid[0]) and grid[test_pos.row][test_pos.col] == elevation + 1:
                     seen.add(test_trail)
                     work.append((test_trail, elevation + 1))
-        return score
+        return len(unique_ends), rating
 
-    def part2(self) -> str:
+    def score_and_rating(self) -> tuple[int, int]:
         grid = self.parse()
         score = 0
+        rating = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == 0:
-                    score += self.rating(grid, Pos(row, col))
-        return str(score)
+                    s, r = self.score(grid, Pos(row, col))
+                    score += s
+                    rating += r
+        return score, rating
+
+    def part1(self) -> str:
+        return str(self.score_and_rating()[0])
+
+    def part2(self) -> str:
+        return str(self.score_and_rating()[1])
 
     def parse(self) -> list[list[int]]:
         return [list(map(int, line)) for line in self.data_lines()]
