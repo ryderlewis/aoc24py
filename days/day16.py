4650646487398648c5a8cac1b1dbeeb7f2246b76
Ryder Lewis <ryder.lewis@gmail.com>
2024-12-19 22:03:59 -0800

    day 18, and 16 part 1

--- days/day16.py
+++ days/day16.py
@@ -11,22 +11,25 @@ class Day16(Day):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def _find_start(self, grid: tuple[tuple[str, ...], ...]) -> Pos:
+    @staticmethod
+    def _find_start(grid: tuple[tuple[str, ...], ...]) -> Pos:
         for row, squares in enumerate(grid):
             for col, square in enumerate(squares):
                 if square == 'S':
                     return Pos(row, col)
         raise RuntimeError("Did not find start")
 
-    def part1(self) -> str:
-        maze = self.parse()
+    def shortest(
+            self,
+            maze: tuple[tuple[str, ...], ...],
+    ) -> int:
         pos = self._find_start(maze)
         work = [(0, pos, EAST)]
         seen = {(pos, EAST): 0}
         while work:
             score, pos, direction = heapq.heappop(work)
             if maze[pos.row][pos.col] == 'E':
-                return str(score)
+                return score
 
             # try going straight
             npos = Pos(pos.row + DIRS[direction][0], pos.col + DIRS[direction][1])
@@ -42,10 +45,50 @@ class Day16(Day):
                     seen[(pos, d)] = score + 1000
                     heapq.heappush(work, (score + 1000, pos, d))
 
-        return "dayXX 1"
+    def part1(self) -> str:
+        return str(self.shortest(self.parse()))
+
+    def all_the_shortest(
+            self,
+            maze: tuple[tuple[str, ...], ...],
+            target: int,
+            steps: set[Pos],
+            score: int,
+            pos: Pos,
+            direction: int,
+            answer: set[Pos]
+    ) -> None:
+        my_steps = {pos}
+        my_steps.update(steps)
+        if score > target:
+            return
+        elif score == target:
+            if maze[pos.row][pos.col] == 'E':
+                answer.update(my_steps)
+                print(f"found one - {len(answer)=}")
+            return
+
+        # try going straight
+        npos = Pos(pos.row + DIRS[direction][0], pos.col + DIRS[direction][1])
+        if npos not in my_steps:
+            if maze[npos.row][npos.col] != '#':
+                self.all_the_shortest(maze, target, my_steps, score+1, npos, direction, answer)
+
+        # and turn left/right
+        for delta in (-1, 1):
+            d = (direction + delta) % len(DIRS)
+            npos = Pos(pos.row + DIRS[d][0], pos.col + DIRS[d][1])
+            if npos not in my_steps:
+                if maze[npos.row][npos.col] != '#':
+                    self.all_the_shortest(maze, target, my_steps, score+1001, npos, d, answer)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        target = self.shortest()
+        maze = self.parse()
+        pos = self._find_start(maze)
+        answer: set[Pos] = set()
+        self.all_the_shortest(maze, target, set(), 0, pos, EAST, answer)
+        return str(len(answer))
 
     def parse(self) -> tuple[tuple[str, ...], ...]:
         return tuple([tuple(line) for line in self.data_lines()])
--- days/day18.py
+++ days/day18.py
@@ -1,12 +1,48 @@
 from .day import Day
+from collections import namedtuple, deque
+
+Pos = namedtuple('Pos', 'x y')
 
 
 class Day18(Day):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def solve(grid: list[list[str]]) -> int:
+        pos = Pos(0, 0)
+        end = Pos(70, 70)
+        work = deque([(0, pos)])
+        seen = {pos}
+        while work:
+            dist, pos = work.popleft()
+            if pos == end:
+                return dist
+            for dy, dx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                npos = Pos(pos.x + dx, pos.y + dy)
+                if npos not in seen and 0 <= npos.x <= 70 and 0 <= npos.y <= 70 and grid[npos.y][npos.x] != '#':
+                    seen.add(npos)
+                    work.append((dist+1, npos))
+        return -1
+
     def part1(self) -> str:
-        return "dayXX 1"
+        grid = [['.' for _ in range(71)] for _ in range(71)]
+        for p in self.parse()[:1024]:
+            grid[p.y][p.x] = '#'
+        return str(self.solve(grid))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        positions = self.parse()
+        grid = [['.' for _ in range(71)] for _ in range(71)]
+        for i, p in enumerate(positions):
+            grid[p.y][p.x] = '#'
+            if i > 1024:
+                if self.solve(grid) < 0:
+                    return str(p)
+
+    def parse(self) -> list[Pos]:
+        ret = []
+        for line in self.data_lines():
+            x, y = line.split(',')
+            ret.append(Pos(int(x), int(y)))
+        return ret
