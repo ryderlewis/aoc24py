d92a8968bd078d8be399851c5425fd5013c98650
Ryder Lewis <ryder.lewis@gmail.com>
2025-01-03 15:02:37 -0800

    day 23 part 2

--- days/day23.py
+++ days/day23.py
@@ -19,14 +19,45 @@ class Day23(Day):
 
     def part2(self) -> str:
         connections = self.parse()
-        largest = sorted(connections.keys(), key=lambda x: -len(connections[x]))
-        for k in largest:
-            print(f"{k}: {len(connections[k])}")
-        return "dayXX 2"
+        return ','.join(sorted(self.biggest_group(connections)))
 
-    @staticmethod
-    def biggest_group(connections: dict[str, set[str]], group: set[str]) -> set[str]:
-        pass
+    def biggest_group(self, connections: dict[str, set[str]]) -> set[str]:
+        output = {}
+        self.make_groups(connections, output)
+        biggest = sorted(output.keys())[-1]
+        for k in sorted(output.keys()):
+            v = output[k]
+            print(f"{k}: {len(v)}")
+        return set(output[biggest].pop())
+
+    def make_groups(
+            self,
+            connections: dict[str, set[str]],
+            output: dict[int, set[tuple[str, ...]]],
+            size: int = 1,
+    ) -> None:
+        for combo in combinations(sorted(connections.keys()), size):
+            union = connections[combo[0]] | {combo[0]}
+            for c in combo[1:]:
+                union &= (connections[c] | {c})
+
+            if len(union) >= size:
+                if size >= 13:
+                    un = tuple(sorted(union))
+                    print(f"{combo=}, {un=}")
+                changed = False
+                if size not in output:
+                    output[size] = set()
+                for c2 in combinations(union, size):
+                    t = tuple(sorted(c2))
+                    if t not in output[size]:
+                        changed = True
+                        output[size].add(t)
+                        if size >= 12:
+                            print(f"{size=}, {len(output[size])=}")
+                if changed:
+                    self.make_groups({k: connections[k] & union for k in union}, output, size+1)
+
 
     def parse(self) -> dict[str, set[str]]:
         ret: dict[str, set[str]] = defaultdict(set)
